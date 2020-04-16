aa9e5a66f7427a10d8241635028bb34373649243
Evgeny Galitskiy <gha665@gmail.com>
2020-04-15 20:54:32 -0400

    modified signup and login forms

--- src/components/authentication/SignUpForm.js
+++ src/components/authentication/SignUpForm.js
@@ -5,10 +5,10 @@ export default function SignUpForm({ handleSignupInput, handleSignupSubmit }) {
     return (
         <div>
             <form onSubmit={handleSignupSubmit}>
-                <label><input name="username" placeholder="Your first name..." onChange={handleSignupInput}/></label>
                 <label><input name="firstName" placeholder="Your first name..." onChange={handleSignupInput}/></label>
                 <label><input name="lastName" placeholder="Your last name..." onChange={handleSignupInput}/></label>
-                <label><input name="password" placeholder="Your password..." onChange={handleSignupInput}/></label>
+                <label><input name="email" placeholder="Your email..." onChange={handleSignupInput}/></label>
+                <label><input name="password" type="password" placeholder="Your password..." onChange={handleSignupInput}/></label>
                 <button>Sign Up</button>
             </form>
         </div>
