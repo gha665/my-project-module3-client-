5dfc5434fa09b342ef945a0df7cce1b14f3cb50e
Evgeny Galitskiy <gha665@gmail.com>
2020-05-07 00:36:25 -0400

    minor styling to home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import SignUpForm from "../authentication/forms/SignUpForm";
 import LoginForm from "../authentication/forms/LoginForm";
+// import "../../pics/eventPics";
 
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,6 +14,8 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
     "& > *": {
       margin: theme.spacing(5),
       width: theme.spacing(40),
@@ -45,15 +48,20 @@ export default function HomePage(props) {
       <h3>What's your party?</h3>
 
       <div className={classes.root}>
-        <Paper elevation={1} className={classes.conference}>
+        <Paper elevation={10} className={classes.conference}>
           <Link to="events/conference">Conference</Link>
+          <br></br>
+          <img href="" alt="conference-icon"></img>
         </Paper>
-
-        <Paper elevation={1} className={classes.wedding}>
+        <Paper elevation={10} className={classes.wedding}>
           <Link to="events/wedding">Wedding</Link>
+          <br></br>
+          <img href="" alt="wedding-icon"></img>
         </Paper>
-        <Paper elevation={1} className={classes.birthday}>
+        <Paper elevation={10} className={classes.birthday}>
           <Link to="events/birthday">Birthday</Link>
+          <br></br>
+          <img href="" alt="birthday-icon"></img>
         </Paper>
       </div>
 
--- src/components/private/PrivatePage.js
+++ src/components/private/PrivatePage.js
@@ -14,6 +14,8 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
     "& > *": {
       margin: theme.spacing(5),
       width: theme.spacing(40),
@@ -51,15 +53,20 @@ export default class PrivatePage extends Component {
         <h3>What's your party?</h3>
 
         <div className={classes.root}>
-          <Paper elevation={1} className={classes.conference}>
+          <Paper elevation={10} className={classes.conference}>
             <Link to="events/conference">Conference</Link>
+            <br></br>
+            <img href="" alt="conference-icon"></img>
           </Paper>
-
-          <Paper elevation={1} className={classes.wedding}>
+          <Paper elevation={10} className={classes.wedding}>
             <Link to="events/wedding">Wedding</Link>
+            <br></br>
+            <img href="" alt="wedding-icon"></img>
           </Paper>
-          <Paper elevation={1} className={classes.birthday}>
+          <Paper elevation={10} className={classes.birthday}>
             <Link to="events/birthday">Birthday</Link>
+            <br></br>
+            <img href="" alt="birthday-icon"></img>
           </Paper>
         </div>
 
